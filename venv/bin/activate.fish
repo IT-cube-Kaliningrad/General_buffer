245d3661cf4e21ce79ea2f087d54977ff459c941
Kardfox <kaleo.asd@gmail.com>
2021-04-09 12:39:58 +0300

    Изменены файлы виртуального окружения

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kardfox/Рабочий стол/IT-Cube_Project/Teacher_files/venv"
+set -gx VIRTUAL_ENV "/home/kardfox/Рабочий стол/IT-Cube_Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
