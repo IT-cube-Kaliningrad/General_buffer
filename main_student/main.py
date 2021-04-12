b22b2885482dacb4bceeb466193ee1a4c7872c9c
Kardfox <kaleo.asd@gmail.com>
2021-04-12 18:46:15 +0200

    Завершена работа над userapi для студентов

--- main_student/main.py
+++ main_student/main.py
@@ -10,9 +10,11 @@ import time
 
 text = None
 
-
 def new_data():
-    label_data['text'] = text
+    if len(text) > 60:
+        label_data['text'] = text[:60] + '...'
+    else:
+        label_data['text'] = text
     root.wm_attributes('-alpha', 1.0)
 
 def get_data():
@@ -30,13 +32,14 @@ def get_data():
                 print(e)
                 messagebox.showerror(title='Ошибка', message='Не удается подключится к хосту',)
                 root.destroy()
+        time.sleep(1)
 
 def copy():
     pyperclip.copy(text)
     root.wm_attributes('-alpha', 0.0)
 
 def main():
-    global root, label_data, SERVER_IP, SERVER_PORT, SCREEN_WIDTH
+    global root, label_data, SERVER_IP, SERVER_PORT
 
     config = configparser.ConfigParser()
     config.read('main_settings.conf')
@@ -45,23 +48,20 @@ def main():
 
     root = Tk()
     root.title('Main')
-    root.overrideredirect(True)
     SCREEN_WIDTH = root.winfo_screenwidth()
-    root.config(bg='white')
     root.wm_attributes('-topmost', True)
     root.wm_attributes('-alpha', 0.0)
-    root.geometry(f'300x100+{SCREEN_WIDTH - 300}+0')
-    
+    root.geometry(f'300x100+{root.winfo_screenwidth() - 300}+0')
 
-    label_data = Label(root, justify='left', bg='white')
-    label_data.pack()
+    label_data = Label(root, justify='left')
+    label_data.grid(row=0, column=0, sticky='W')
 
     button_copy_data = Button(root, text='Копировать', command=copy)
-    button_copy_data.pack(expand=True, anchor=SW)
+    button_copy_data.grid(row=1, column=0, sticky='SW')
 
     thread_data = threading.Thread(target=get_data, daemon=True)
     thread_data.start()
-    
+
     root.mainloop()
 
 if __name__ == '__main__':
--- start.bat
+++ start.bat
@@ -5,7 +5,7 @@
     set FLASK_ENV=development
     set FLASK_DEBUG=0
 :   SERVER
-    set SERVER_HOST=192.168.0.3
+    set SERVER_HOST=127.0.0.1
     set FLASK_RUN_PORT=5000
 
 flask run --host %SERVER_HOST%
