99f4e341fae45d720122e0e501599e332461ee59
Kardfox <kaleo.asd@gmail.com>
2021-05-12 16:42:31 +0300

    Внесены изменения в структуру проекта для добавления кроссплатформенности. Изменен внешний вид приложения для учителя

--- apps/app_teacher/app.py
+++ apps/app_teacher/app.py
@@ -1,10 +1,10 @@
 if __name__ == "__main__":
     try:
-        app = __import__("app_class_teacher")
+        app = __import__("app_class")
         app.App()
     except KeyboardInterrupt:
         print("Приложение принудительно остановлено")
     except ModuleNotFoundError:
-        print("Не обнаружен файл app_class_teacher.cp39-win_amd64.pyd")
-    except ImportError:
-        print("Не обнаружен файл app_class_teacher.c")
+        print("Не обнаружен файл app_class.pyc")
+    except Exception as e:
+        print(f"Неизвестная ошибка\nError: {e}")
