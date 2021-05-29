15ab3495cd8a4fcb2ac9b7e140c81e7a14e91e33
Kardfox <kaleo.asd@gmail.com>
2021-05-29 17:06:03 +0300

    Добавлена система уведомлений пользователя об ошибках. Оптимизирована система проверки подключения со стороны учителя и студента. Подправлен README

--- apps/app_student/app.py
+++ apps/app_student/app.py
@@ -3,22 +3,33 @@ import tkinter.messagebox as msg
 import socket
 import configparser
 import threading
+import os
 
+
+def warning(message):
+    msg.showwarning("Предупреждение", message)
+
+def error(message, error=None):
+    msg.showerror("Ошибка", message)
+    print(error)
 
 class Client(socket.socket):
-    def __init__(self, host, port, id=None):
-        address = host, port
-        super().__init__(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, host, port, warning_disconnect=0):
+        self.warning_disconnect = warning_disconnect
+        self.address = host, port
         try:
-            self.connect(address)
-            print("ID:", self.getsockname()[1])
-        except (ConnectionRefusedError, BrokenPipeError) as e:
-            print(e, f"\nНе удается подключиться к серверу {':'.join(map(str, address))}")
+            super().__init__(socket.AF_INET, socket.SOCK_STREAM)
+            self.connect(self.address)
+        except ConnectionError as e:
+            print(f"{e}\nНе удается подключиться к серверу {':'.join(map(str, self.address))}")
+            if self.warning_disconnect:
+                warning(f"Не удается подключиться к серверу {':'.join(map(str, self.address))}.\nВозможно, этот сервер не существует или превысили макс. кол-во подлкючений на нем")
             exit()
+        print("ID:", self.getsockname()[1])
     
-    def get_data(self, size=1024):
+    def get_data(self, size=2048):
         data = self.recv(size).decode("utf-8")
-        if data and not "¤" in data:
+        if data:
             return data
         return None
 
@@ -26,29 +37,32 @@ class App:
     def __init__(self):
         self.bufer_text = "NULL"
 
-        config = configparser.ConfigParser()
-        config.read("settings.conf")
+        self.config = configparser.ConfigParser()
+        self.config.read("settings.conf")
 
         try:
-            SERVER_PORT = int(config["SERVER"]["SERVER_PORT"])
-            SERVER_IP = config["SERVER"]["SERVER_IP"]
+            self.SERVER_PORT = int(self.config["SERVER"]["SERVER_PORT"])
+            self.SERVER_IP = self.config["SERVER"]["SERVER_IP"]
 
-            self.AUTOCOPY = int(config["APP"]["AUTOCOPY"])
+            self.AUTOCOPY = int(self.config["APP"]["AUTOCOPY"])
 
-            self.client = Client(SERVER_IP, SERVER_PORT)
+            self.SERVER_DISCONNECTION = int(self.config["SHOW_WARNINGS"]["SERVER_DISCONNECTION"])
+
+            self.client = Client(self.SERVER_IP, self.SERVER_PORT, warning_disconnect=self.SERVER_DISCONNECTION)
         except KeyError as e:
-            self._error("Неправильно составлен или отсутствует файл settings.conf", error=e)
+            error("Неправильно составлен или отсутствует файл settings.conf", error=e)
         except ValueError as e:
-            self._error("Неправильное значение параметров в файле settings.conf", error=e)
+            error("Неправильное значение параметров в файле settings.conf", error=e)
 
         self.root = tk.Tk()
+        root = self.root
+        root.title("App")
+        root.geometry("300x0")
+        root.wm_attributes("-topmost", True)
         if not self.AUTOCOPY:
-            root = self.root
-            root.title("App")
             root.columnconfigure(0, weight=True)
             root.rowconfigure(0, weight=True)
             root.geometry(f"300x150+{root.winfo_screenwidth() - 300}+0")
-            root.wm_attributes("-topmost", True)
             root.withdraw()
 
             self.text_data = tk.Text(self.root)
@@ -58,19 +72,25 @@ class App:
 
             tk.Button(root, text="Копировать", command=self._copy).grid(row=1, column=0, sticky="WE")
 
-            threading.Thread(target=self._auto_data, daemon=True).start()
+        threading.Thread(target=self._run_client, daemon=True).start()
+        root.mainloop()
 
-            root.mainloop()
-            exit()
-
-        self._auto_data()
-        
-    def _auto_data(self):
+    def _run_client(self):
         while True:
-            data = self.client.get_data()
-            if data and data != self.bufer_text:
-                self.bufer_text = data
-                (self._show, self._copy)[self.AUTOCOPY]()
+            try:
+                self.client.send(b"check")
+                data = self.client.get_data() or self.bufer_text
+                if not "¤" in data and data != self.bufer_text:
+                    self.bufer_text = data
+                    if self.AUTOCOPY:
+                        self.root.clipboard_append(self.bufer_text)
+                    else:
+                        self._show()
+            except ConnectionError as e:
+                print(f"{e}\nНе удается подключиться к серверу")
+                self.client.close()
+                warning(f"Не удается подключиться к серверу")
+                os._exit(1)
 
     def _show(self):
         self.root.lift()
@@ -83,18 +103,11 @@ class App:
     def _copy(self):
         self.root.clipboard_append(self.bufer_text)
         self.root.withdraw()
-    
-    def _error(self, message, error=None):
-        msg.showerror("Ошибка", message)
-        print(error)
 
-def main():
+if __name__ == "__main__":
     try:
         App()
     except KeyboardInterrupt:
         print("\nПриложение принудительно остановлено")
     except Exception as e:
-        print(f"Неизвестная ошибка\nError: {e}")
-
-if __name__ == "__main__":
-    main()
+        print(f"Ошибка: {e}")
